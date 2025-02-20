b2fe09a918ba67ce23ccc1b68105a361fbaa495c
JayMar921 <jayharronabejar@gmail.com>
2025-02-21 00:38:13 +0800

    Tilemap Handler Initial Commit

---
+++ pixel-game-app/src/handlers/TilemapGenerator.js
@@ -0,0 +1,150 @@
+import { CanvasScreen, Sprite } from "@jaymar921/2dgraphic-utils";
+
+export default class TilemapGenerator {
+  /**
+   *
+   * @param {CanvasScreen} CanvasScreen
+   */
+  constructor(CanvasScreen) {
+    this.canvasScreen = CanvasScreen;
+    this.levels = [];
+  }
+
+  addLevel(
+    tileMapImageSource,
+    mapWidth,
+    mapHeight,
+    offset = { x: 0, y: 0 },
+    colliders = []
+  ) {
+    const levelSprite = new Sprite({
+      objID: `level-${this.levels.length + 1}`,
+      name: `level-${this.levels.length + 1}`,
+      posX: offset.x,
+      posY: offset.y,
+      imageSource: tileMapImageSource,
+    });
+
+    const colliderSprites = [];
+
+    for (let y = 0; y < colliders.length; y++) {
+      for (let x = 0; x < colliders[y].length; x++) {
+        if (colliders[y][x] === 0) continue;
+        const posX = x * (mapWidth / colliders[y].length);
+        const posY = y * (mapHeight / colliders.length);
+        const colliderSprite = new Sprite({
+          objID: `collider-${posX}-${posY}`,
+          name: "id-" + colliders[y][x],
+          posX,
+          posY,
+        });
+        colliderSprites.push(colliderSprite);
+      }
+    }
+
+    this.levels.push({
+      level: this.levels.length + 1,
+      map: levelSprite,
+      colliders: colliderSprites,
+      mapWidth,
+      mapHeight,
+      boxSize: mapHeight / colliders.length,
+    });
+
+    return this;
+  }
+
+  loadLevel(n) {
+    const level = this.levels[n - 1];
+    if (!level) return;
+
+    this.canvasScreen.registerObject(level.map);
+
+    for (const collider of level.colliders) {
+      this.canvasScreen.registerObject(collider);
+    }
+
+    return this;
+  }
+
+  zoomInAnimation(from, afterAnimationCallback = () => {}, speed = 0.008) {
+    setTimeout(() => {
+      let scale = from;
+
+      const { width, height } = CanvasScreen.screen;
+      const id = setInterval(async () => {
+        if (!this.canvasScreen) return;
+        if (scale >= 1) {
+          clearInterval(id);
+          afterAnimationCallback();
+          return;
+        }
+
+        const globalScale = this.canvasScreen.globalScale;
+
+        this.canvasScreen.setGlobalScale((scale += speed));
+        const { x, y } = CanvasScreen.cameraOffset;
+
+        const centerX =
+          (CanvasScreen.fixedCameraOffset.x +
+            (CanvasScreen.fixedCameraOffset.x + width)) /
+          2;
+        const centerY =
+          (CanvasScreen.fixedCameraOffset.y +
+            (CanvasScreen.fixedCameraOffset.y + height)) /
+          2;
+
+        const newX = centerX - (centerX - x) / (scale / globalScale);
+        const newY = centerY - (centerY - y) / (scale / globalScale);
+
+        this.canvasScreen.setCameraOffset(newX, newY);
+      }, 10);
+    }, 100);
+    return this;
+  }
+
+  getBoxCollider(n) {
+    const level = this.levels[n - 1];
+    if (!level) return;
+
+    const colliders = [];
+    for (const collider of level.colliders) {
+      colliders.push({
+        x: collider.posX,
+        y: collider.posY,
+        width: level.boxSize,
+        height: level.boxSize,
+        name: collider.name,
+      });
+    }
+
+    return colliders;
+  }
+
+  static convertSingleArrayMapTo2DArray = (
+    arr = [],
+    dimension = { row: 0, column: 0 },
+    value = undefined
+  ) => {
+    const newArr = [];
+
+    let i = 0;
+
+    for (let r = 0; r < dimension.row; r++) {
+      const colArr = [];
+
+      for (let c = 0; c < dimension.column; c++) {
+        let val = arr[i++];
+
+        if (val !== null || val !== undefined) {
+          if (value !== undefined && val !== 0) val = value;
+          colArr.push(val);
+        }
+      }
+
+      newArr.push(colArr);
+    }
+
+    return newArr;
+  };
+}
