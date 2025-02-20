2e64a1fcfb12faa601aa7cf42203059d7ab08a6d
JayMar921 <jayharronabejar@gmail.com>
2025-02-21 00:38:31 +0800

    World Loading

--- pixel-game-app/src/App.jsx
+++ pixel-game-app/src/App.jsx
@@ -1,15 +1,50 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { useCanvas } from "./hooks/useCanvas";
+import TilemapGenerator from "./handlers/TilemapGenerator";
+import { world_collider, world_map_img_source } from "./constants/world_data";
+import { CanvasScreen } from "@jaymar921/2dgraphic-utils";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const canvasScreen = useCanvas(
+    "canvas-screen",
+    window.innerWidth,
+    window.innerHeight,
+    "#25220E"
+  );
+  const [world, setWorld] = useState(null);
+
+  const loginPlayer = () => {};
+
+  useEffect(() => {
+    if (!canvasScreen.getScreen() && !world) return;
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+    canvasScreen.enableScreenDrag(true);
+    canvasScreen.enableScreenZoom(true);
+
+    if (!world) {
+      const worldGen = new TilemapGenerator(canvasScreen.getScreen())
+        .addLevel(
+          world_map_img_source,
+          1600,
+          1280,
+          {
+            x: -(1600 / 2 - screenWidth / 2),
+            y: -(1280 / 2 - screenHeight / 2),
+          },
+          TilemapGenerator.convertSingleArrayMapTo2DArray(world_collider)
+        )
+        .loadLevel(1)
+        .zoomInAnimation(0.2, loginPlayer);
+      setWorld(worldGen);
+    }
+  }, [canvasScreen, world]);
 
   return (
     <>
       <div className="flex justify-center place-items-center h-screen">
-        <h1 className="text-2xl h-fit">Pixel Game - v 0.0.1</h1>
+        <canvas className="w-full h-full" id="canvas-screen"></canvas>
       </div>
     </>
   );
