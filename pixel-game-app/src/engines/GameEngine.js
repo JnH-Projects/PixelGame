2c02cde0e6e6380267f8dcc3b365653733ed0096
JayMar921 <jayharronabejar@gmail.com>
2025-02-21 10:34:35 +0800

    Added GameEngine for world generation

---
+++ pixel-game-app/src/engines/GameEngine.js
@@ -0,0 +1,36 @@
+import { CanvasScreen } from "@jaymar921/2dgraphic-utils";
+import TilemapGenerator from "../handlers/TilemapGenerator";
+import { world_collider, world_map_img_source } from "../constants/world_data";
+
+export default class GameEngine {
+  /**
+   *
+   * @param {CanvasScreen} canvasScreen
+   */
+  constructor(canvasScreen) {
+    this.canvasScreen = canvasScreen;
+    this.world = new TilemapGenerator(canvasScreen);
+  }
+
+  initializeWorld(onLoadCallback = () => {}) {
+    const { width: screenWidth, height: screenHeight } =
+      this.canvasScreen.canvasElement;
+
+    // initialize world
+    this.world
+      .addLevel(
+        world_map_img_source,
+        1600,
+        1280,
+        {
+          x: -(1600 / 2 - screenWidth / 2),
+          y: -(1280 / 2 - screenHeight / 2),
+        },
+        TilemapGenerator.convertSingleArrayMapTo2DArray(world_collider)
+      )
+      .loadLevel(1)
+      .zoomInAnimation(0.2, onLoadCallback);
+
+    return this;
+  }
+}
